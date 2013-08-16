c2a93a6bb7b63df6d947f001b7fdca27be6a05a2
Marco Badan <marco.badan@gmail.com>
2013-08-16 19:24:31 +0200

    removed repeated keyword argument in setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,6 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 setup(
     name='cmsplugin-googleplus',
     version='0.1',
-    packages=['cmsplugin_googleplus'],
     include_package_data=True,
     license='BSD License',  # example license
     description='Django-CMS plugin for Google Plus Activities',
