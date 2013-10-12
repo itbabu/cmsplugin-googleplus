6492f423ba363e91c0911078e8cec24dc17ecc08
Marco Badan <marco.badan@gmail.com>
2013-10-12 15:37:37 +0200

    updated version 0.3.4

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='cmsplugin-googleplus',
-    version='0.3.3',
+    version='0.3.4',
     include_package_data=True,
     license='MIT License',
     description='Django-CMS plugin for Google Plus Activities',
